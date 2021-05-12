809bd644c9d3b58705b18c5696fc9d8880ae0fdf
Nicolás Castellán <74460331+nico-castell@users.noreply.github.com>
2021-05-12 16:15:56 -0300

    ❌ Removed git instructions from vscode.sh

--- vscode.sh
+++ vscode.sh
@@ -5,7 +5,6 @@
 # from fedora_setup.sh because it depends on variables declared in
 # fedora_setup.sh
 
-[[ ${TO_DNF[@]} == *"git"* ]]        && LIST+=("Git with vscode")
 [[ ${APPEND_DNF[@]} == *"nodejs"* ]] && LIST+=("VS Code Extension development")
 
 # Do not run if the package is not being sourced
@@ -16,58 +15,6 @@ printf "Choose some developer tools to prepare:\n"
 
 select c in "${LIST[@]}" exit; do
 case $c in
-	"Git with vscode")
-	# Configure user to make commits.
-	echo "Configure git:"
-	read -p "What's your GitHub username? " USERNAME
-	git config --global user.name "$USERNAME"
-	read -p "What's your GitHub email? " EMAIL
-	git config --global user.email "$EMAIL"
-
-	read -p "What do you want to call the default branch? " DEF_BRANCH
-	if [ ! -z $DEF_BRANCH ]; then
-		git config --global init.defaultBranch "$DEF_BRANCH"
-	fi
-	unset USERNAME EMAIL DEF_BRANCH
-
-	# Integrate vscode in some common Git operations.
-	printf "Please, select a default editor for commit messages:\n"
-	GIT_EDITORS+=("vscode")
-	GIT_EDITORS+=("vim")
-	GIT_EDITORS+=("nano")
-	GIT_EDITORS+=("gedit")
-	select GIT_EDITOR in ${GIT_EDITORS[@]}; do
-	case $GIT_EDITOR in
-		vim)    git config --global core.editor vim            ;;
-		vscode) git config --global core.editor 'code --wait'  ;;
-		nano)   git config --global core.editor nano           ;;
-		gedit)  git config --global core.editor 'gedit -s'     ;;
-		*) echo "Option $GIT_EDITOR not recognized."; continue ;;
-	esac; break; done
-	unset GIT_EDITOR GIT_EDITORS
-
-	printf "Setting \e[01mVS Code\e[00m as the default merge tool...\n"
-	git config --global merge.tool vscode
-	git config --global mergetool.vscode.cmd 'code --wait $MERGED'
-	git config --global diff.tool vscode
-	git config --global difftool.vscode.cmd 'code --wait --diff $LOCAL $REMOTE'
-
-	echo "Configuring pull behaviour..."
-	git config --global pull.ff only
-
-	# Set up aliases
-	printf "Setting up some Git aliases...\n"
-	git config --global alias.mrc '!git merge $1 && git commit -m "$2" --allow-empty && :'
-	git config --global alias.flog "log --all --graph --oneline --format=format:'%C(bold yellow)%h%C(r) %an➜ %C(bold)%s%C(r) %C(auto)%d%C(r)\'"
-	git config --global alias.sflog "log --all --graph --oneline --format=format:'%C(bold yellow)%h%C(r) §%C(bold green)%G?%C(r) %an➜ %C(bold)%s%C(r) %C(auto)%d%C(r)'"
-	git config --global alias.slog 'log --show-signature -1'
-	git config --global alias.mkst 'stash push -u'
-	git config --global alias.popst 'stash pop "stash@{0}" -q'
-	git config --global alias.unstage 'reset -q HEAD -- .'
-
-	echo
-	;;
-
 	"VS Code Extension development")
 	printf "Installing: \e[01myo generator-code vsce\e[00m...\n"
 	Animate & PID=$!
