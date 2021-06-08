67720b1a52d7b37872afbd557441d2ccd8d22c17
Nicolás Castellán <cnicolas.developer@gmail.com>
2021-06-08 20:16:38 -0300

    🔨 Added Golang config for all users

--- post-install.d/golang.sh
+++ post-install.d/golang.sh
@@ -1,25 +1,13 @@
 # bash script to be sourced from fedora_setup.sh
 
-# Modify $GOPATH for the Z-Shell
-mkdir -p ~/.zshrc.d && \
-	printf "
-# Configure Golang
-# Find folder in case of custom installation
-[ -d /usr/local/go/bin ] && \
-	PATH=\"/usr/local/go/bin:\$PATH\"
+# Add Golang configs every user's profile
+[ ! -f /etc/profile.d/golang.sh ] && \
+	printf "# Configure custom Golang folders
 
-# \$GOPATH and built binaries
-export GOPATH=\"\$HOME/.local/golang\"
-export PATH=\"\$GOPATH/bin:\$PATH\"\n" | tee -a ~/.zshrc.d/golang.sh >/dev/null
+[ -d /usr/local/go/bin ] && \\
+	export PATH=\"/usr/local/go/bin:\$PATH\"
 
-# Modifiy $GOPATH for the Bourne Again shell
-mkdir -p ~/.bashrc.d && \
-	printf "
-# Configure Golang
-# Find foler in case of custom installation
-[ -d /usr/local/go/bin ] && \
-	PATH=\"/usr/local/go/bin:\$PATH\"
+GOPATH=\"\$HOME/.local/golang\"
+PATH=\"\$GOPATH/bin:\$PATH\"
 
-# \$GOPATH and built binaries
-export GOPATH=\"\$HOME/.local/golang\"
-export PATH=\"\$GOPATH/bin:\$PATH\"\n" | tee ~/.bashrc.d/golang.sh >/dev/null
+export GOPATH PATH\n" | sudo tee /etc/profile.d/golang.sh >/dev/null
