6a6283a8d8ef3ae2eeecfa08801dd41fbda10148
Nicolás Castellán <cnicolas.developer@gmail.com>
2021-08-03 20:21:16 -0300

    ✨ the noip installer script now creates a log

--- scripts/duc_noip_install.sh
+++ scripts/duc_noip_install.sh
@@ -35,6 +35,7 @@ USAGE_MSG () {
 Options:
 	-n) Don't install a menu entry
 	-p) Override installation prefix
+	-s) Set it up as a systemd service (also selects -n)
 
 Examples:
 	./%s
@@ -43,6 +44,8 @@ Examples:
 
 # Prepare variables
 integrate_desktop=yes
+systemd_setup=no
+logfile="/usr/local/src/duc-noip-install-log.txt"
 prefix="$HOME/.local"
 [ $(id -u) == 0 ] && prefix="/usr/local"
 
@@ -51,12 +54,24 @@ while [ -n "$1" ]; do
 	case "$1" in
 		-n) integrate_desktop=no ;;
 		-p) prefix="$2"; shift   ;;
+		-s) systemd_setup=yes    ;;
 		*) USAGE_MSG >&2; exit 0 ;;
 esac; shift; done
 unset USAGE_MSG
 
+# Don't integrate with the desktop when setting up the DUC as a systemd service. Why? Because noip2
+# has different behaviour when running as a systemd daemon (service) and when running as its own
+# process, and if both are running, they may interfere with each other.
+[ "$systemd_setup" = "yes" ] && integrate_desktop=no
+
 # Acquire root privileges now
 sudo echo >/dev/null || exit 4
+
+sudo printf "User choices when installing:
+  - Integrating desktop: %s
+  - Setting up as systemd service: %s
+  - Installation prefix: %s
+" "$integrate_desktop" "$systemd_setup" "$prefix" > "$logfile"
 
 # Greet the user
 printf "Installing \e[01;36mNo-IP's Dynamic Update Client\e[00m (DUC):
@@ -78,8 +93,63 @@ else
 	status=bad
 fi
 
+sudo printf "status after running makefile = %s\n" "$status" > "$logfile"
+
+installation_result=$status
+
+# Integrate in systemd using service and timer
+if [ "$systemd_setup" = "yes" -a "$installation_result" = "ok" ]; then
+	systemd_dir="/etc/systemd/system"
+	systemd_service="$systemd_dir/duc-noip-local.service"
+	systemd_timer="$systemd_dir/duc-noip-local.timer"
+
+	# Ensure the directory is present and write config files
+	sudo mkdir -p "$systemd_dir"
+	# Write service file
+	sudo cat <<EOF > "$systemd_service" || status=bad
+[Unit]
+Description=No-IP's Dynamic Update Client
+Requires=NetworkManager.service
+After=NetworkManager-wait-online.service
+Wants=duc-noip-local.timer
+
+[Service]
+Type=oneshot
+ExecStart=noip2
+
+[Install]
+WantedBy=multi-user.target
+EOF
+	# Write timer file
+	sudo cat <<EOF > "$systemd_timer" || status=bad
+[Unit]
+Description=No-IP's Dynamic Update Client's timer
+Requires=NetworkManager.service
+After=NetworkManager-wait-online.service
+
+[Timer]
+Unit=duc-noip-local.service
+OnCalendar=*-*-* *:00,30:00
+OnBootSec=10s
+
+[Install]
+WantedBy=timers.target
+EOF
+
+	sudo printf "systemd files written:
+  - %s
+  - %s
+" "$systemd_service" "$systemd_timer" > "$logfile"
+
+	# Reload daemons and enable the timer to run the service
+	sudo systemctl daemon-reload
+	sudo systemctl enable --now duc-noip-local.timer
+
+	unset systemd_dir systemd_service systemd_timer
+fi
+
 # Integrate with the user's desktop
-if [ "$integrate_desktop" = "yes" -a "$status" = "ok" ]; then
+if [ "$integrate_desktop" = "yes" -a "$installation_result" = "ok" ]; then
 	# Create relevant directories and files
 	target="$prefix/bin"
 	entry="$prefix/share/applications"
@@ -171,7 +241,20 @@ Name=Open to end process
 Exec=noip-assist -k
 EOF
 	chmod 644 "$entry"
+
+	sudo printf "Files written to integrate with the desktop:
+- %s
+- %s
+- %s
+" "$target" "$entry" "$icon" > "$logfile"
 fi
+
+sudo printf "Final status after installation: %s\n" "$status" > "$logfile"
+sudo printf "If you want to remove noip's DUC, you should delete the previously listed files, and:
+  - /usr/local/bin/noip2       (executable binary)
+  - /usr/local/etc/no-ip2.conf (config file)
+  - This list may not be complete, so the system admin might have to check for leftovers
+" > "$logfile"
 
 if [ "$status" = "ok" ]; then
 	printf "\e[01;32mSuccess!\e[00m The script ran successfully, and the DUC is installed"
